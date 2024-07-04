cd3740c73badb3b9fd4183385fec5408649bd7f9
GusEngers <gustavoengers6441@gmail.com>
2024-07-04 13:03:10 -0300

    FIX: Añadir configuración de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const api = require('./api/routes/index.js');
 const handlers = require('./utils/handlers.js');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use('/api', api);
 app.use(handlers.handleError);
 app.use(handlers.handleNotFound);
